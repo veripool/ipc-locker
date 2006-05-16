e140bd9d27ea18e993ccf27783be6a78dfc48b8d
Wilson Snyder <wsnyder@wsnyder.org>
2006-05-16 18:43:27 +0000

    Version bump
    git-svn-id: file://localhost/svn/perltools/IPC-Locker/trunk/IPC-Locker@42 5359c949-c622-0410-b959-aa3a61b3b029

--- lib/IPC/Locker.pm
+++ lib/IPC/Locker.pm
@@ -241,7 +241,7 @@ use Carp;
 # Other configurable settings.
 $Debug = 0;
 
-$VERSION = '1.440';
+$VERSION = '1.450';
 
 ######################################################################
 #### Useful Globals
